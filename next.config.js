9a4b8e349ac7a290b3f6ab4b3d1be80eb741878a
Rajat Rawat <71941520+rajatraw21@users.noreply.github.com>
2023-06-30 20:22:24 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
 
-module.exports = nextConfig
+module.exports = {
+  target: "experimental-serverless-trace",
+  nextConfig,
+};
